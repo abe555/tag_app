c4be2e243eabb09b4109cb138520d62ffef8b0f5
abe555 <riho.has3v@gmail.com>
2020-10-16 17:22:44 +0900

    Fix post_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,31 @@
 class PostsController < ApplicationController
 
 	def create
+    @post = current_user.posts.new(post_params)
+    tag_list = params[:post][:tag_name].split(nil)
+    if @post.save
+      @post.save_tag(tag_list)
+      redirect_back(fallback_location: root_path)
+    else
+      redirect_back(fallback_location: root_path)
+    end
 	end
 
 	def index
 		@post = Post.all
+		@tag = Tag.all
+		@post = current_user.posts.new
 	end
+
+	def show
+		@post = Post.find(params[:id])
+		@post_tags = @post.tags
+	end
+
+
+	private
+		def post_params
+			params.require(:post).permit(:content)
+		end
 
 end
