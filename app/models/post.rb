f624d6343ae15f0590cd7aab2c04f43e1c7c45f0
abe555 <riho.has3v@gmail.com>
2020-10-17 22:28:08 +0900

    Add post_view & devise_view

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,7 @@ class Post < ApplicationRecord
   has_many :tag_maps, dependent: :destroy
   has_many :tags, through: :tag_maps
 
+
   def save_tag(sent_tags)
   	current_tags = self.tags.pluck(:tag_name) unless self.tags.nil?
   	old_tags = current_tags - sent_tags
