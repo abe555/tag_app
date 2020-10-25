2079c5c0ed77b44f113d1a0fb3ac31294e087ffb
abe555 <riho.has3v@gmail.com>
2020-10-25 17:25:44 +0900

    Fix Routing

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
 
   root "posts#index"
 
-  resource :posts
+  resources :posts
 
-  resource :tags do
+  resources :tags do
   	get 'posts' => 'posts#search'
   end
 
